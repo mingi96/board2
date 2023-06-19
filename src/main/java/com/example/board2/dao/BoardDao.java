409b4cbc039aead0e38de10f693cc00094797a94
mingi96 <minnet1030@naver.com>
2023-06-19 17:42:51 +0900

    spring

--- src/main/java/com/example/board2/dao/BoardDao.java
+++ src/main/java/com/example/board2/dao/BoardDao.java
@@ -1,8 +1,18 @@
 package com.example.board2.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.board2.dto.Board;
 
 @Mapper
 public interface BoardDao {
+	public int maxNum() throws Exception;
 
+	public void insertData(Board board) throws Exception;
+
+	public int getDataCount(String searchKey, String searchValue) throws Exception;
+
+	public List<Board> getLists(String searchKey, String searchValue, int start, int end) throws Exception;
 }
--- src/main/java/com/example/board2/service/BoardService.java
+++ src/main/java/com/example/board2/service/BoardService.java
@@ -1,5 +1,15 @@
 package com.example.board2.service;
 
-public interface BoardService {
+import java.util.List;
 
+import com.example.board2.dto.Board;
+
+public interface BoardService {
+	public int maxNum() throws Exception;
+
+	public void insertData(Board board) throws Exception;
+
+	public int getDataCount(String searchKey, String searchValue) throws Exception;
+
+	public List<Board> getLists(String searchKey, String searchValue, int start, int end) throws Exception;
 }
